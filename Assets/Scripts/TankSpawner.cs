1b34e7b541eb8262318ef6a84a2f49674bed6ab8
Akshay Manthekar <akky123654@gmail.com>
2022-06-15 11:21:34 +0530

    Initial commit

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject tankPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+        Debug.Log("Tank is Spawned");
+    }
+
+    
+}
