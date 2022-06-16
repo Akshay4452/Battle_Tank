24cde2e7d4ec0927ef0b24b45ceb647879016685
Akshay Manthekar <akky123654@gmail.com>
2022-06-16 10:34:06 +0530

    Movement and Rotation applied
    Tank movement and rotation functionalities have been assigned.

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -3,16 +3,32 @@
 public class TankController
 {
    private TankModel tankModel;
-   private TankView tankView;
+   private TankView tankView;  // these are empty referenes here
+
+   private Rigidbody rb; // reference of rigid body
 
    // Adding constructor to the class
    // using below constructor to set up the empty references above
+
+   // Below is class constructor TankController
    public TankController(TankModel _tankModel, TankView _tankView) {
     tankModel = _tankModel;
-    tankView = _tankView;
+    tankView = GameObject.Instantiate<TankView>(_tankView); 
+    rb = tankView.GetRigidBody(); 
+    // the references have been assigned (not empty anymore)
+
     tankModel.SetTankController(this);
     tankView.SetTankController(this); // this => reference of current script (TankController)
 
-    GameObject.Instantiate(tankView);
+   }
+
+   public void Move(float movement, float movementSpeed)
+   {
+      rb.velocity = tankView.transform.forward * movement * movementSpeed;
+   }
+
+   public void Rotate(float rotation, float rotationSpeed)
+   {
+
    }
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class TankView : MonoBehaviour
 {
     private TankController tankController;
+
+    private float movement;
+    private float rotation;
+
+    public Rigidbody rb; // 
+    // instantiate here as rigidbody is attached to the prefab and prefab is attached to TankView script
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +24,26 @@ public class TankView : MonoBehaviour
         tankController = _tankController;
     }
 
+    private void Movement()
+    {
+        float movement = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+    }
+
+    public Rigidbody GetRigidBody()
+    {
+        return rb;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+
+        if(movement != 0)
+        {
+            tankController.Move(movement, 30); // Hardcoding movement speed as of now
+        }
+
     }
 }
